f88a43d0559e2825e088d67ac6eaaf3f0c5c208a
Stella McEwen <mcewen.stella@gmail.com>
2019-04-10 00:11:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split("")
   reverse_sentence = []
   sentence_array.each do |word|
-    reverse_sentence.push
+    reverse_sentence.push word.reverse
   end
   reverse_sentence.join("")
 end
