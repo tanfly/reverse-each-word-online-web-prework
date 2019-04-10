fe15b3cdf129792338e69e94624e08db80450fa4
Stella McEwen <mcewen.stella@gmail.com>
2019-04-10 00:04:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   reverse_sentence = []
   sentence.split = sentence_array
   sentence_array.each do |word|
-    puts "#{word.reverse}" 
+    reverse_sentence.push("#{word.reverse}")
   end
   sentence_array
 end
